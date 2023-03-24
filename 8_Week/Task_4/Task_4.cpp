9842f2b9fe1d8cb9631bed8bce0fae98cdfa1acf
Kostiantyn <krokotkoleshnia@gmail.com>
2023-03-24 17:51:16 +0200

    Week_8 commit

---
+++ 8_Week/Task_4/Task_4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+using namespace std;
+
+int Sum(int i, int A[], int n);
+
+int main(void) {
+
+    int A[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9,10 };
+    cout << Sum(0, A, 10);
+
+    return 0;
+}
+int Sum(int i, int A[], int n)
+{
+    if (i == n - 1)
+        return A[i];
+    else
+        return A[i] + Sum(i + 1, A, n);
+}
