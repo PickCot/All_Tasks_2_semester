00c4cd3ddb8c865e57a1e7df6f33651aed90528e
Kostiantyn <krokotkoleshnia@gmail.com>
2023-04-13 14:55:56 +0300

    Week_10

---
+++ 10_Week/Task_4/Task_4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class ShopItemOrder
+{
+public:
+    string name;
+    double price;
+    int itemsOrdered;
+    double totalPrice();
+    ShopItemOrder(string name, int items, double price);
+};
+double ShopItemOrder::totalPrice()
+{
+    return this->itemsOrdered * this->price;
+}
+ShopItemOrder::ShopItemOrder(string name, int items, double price)
+{
+    this->name = name;
+    this->itemsOrdered = items;
+    this->price = price;
+}
+void print(ShopItemOrder* sio)
+{
+    cout << "Consumer ordered " << sio->itemsOrdered << " items of "
+        << sio->name << " for " << sio->price << " dollars each." << endl
+        << "The total sum is " << sio->totalPrice();
+}
+int main()
+{
+    ShopItemOrder sio = ShopItemOrder("Milk", 2, 4);
+    print(&sio);
+
+    return 0;
+}
