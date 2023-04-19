5fce1c4bf232178b98a8028f617563a1939913af
Kostiantyn <krokotkoleshnia@gmail.com>
2023-04-19 11:29:21 +0300

    Week_11

---
+++ 11_Week/11_Week/11_Week.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+class FlightBooking {
+public:
+	FlightBooking(int id, int capacity, int reserved);
+	void printStatus();
+	void set(int id, int capacity, int reserved);
+	void get();
+private:
+	int id;
+	int capacity;
+	int reserved;
+};
+void FlightBooking::printStatus()
+{
+	if (capacity < reserved)
+	{
+		cout << "Error";
+	}
+	else
+	{
+		double percentage = double(this->reserved) / this->capacity * 100;
+
+		cout << "Flight " << this->id << " : " << this->reserved
+		<< "/" << this->capacity << " (" << percentage << "%) seats taken";
+
+	}
+}
+FlightBooking::FlightBooking(int id, int capacity, int reserved)
+{
+	set(id, capacity, reserved);
+	
+}
+void FlightBooking::set(int inputId, int InputCapacity, int inputReserved)
+{
+	this->id = inputId;
+	this->capacity = InputCapacity;
+	this->reserved = inputReserved;
+}
+void FlightBooking::get()
+{
+	cout << "Id : "<< this->id << "Capacity : " << this->capacity << "Reserved : " << this->reserved;
+}
+int main() 
+{
+	int reserved = 0,
+		capacity = 0;
+	std::cout << "Provide flight capacity: ";
+	std::cin >> capacity;
+	std::cout << "Provide number of reserved seats: ";
+	std::cin >> reserved;
+	FlightBooking booking(1, capacity, reserved);
+	booking.printStatus();
+	return 0;
+}
